f3b49d6fe7fd7ae171c75c5293b155e49e6380f4
Rob Minson <rob.minson@mindcandy.com>
2016-01-29 13:20:39 +0000

    WiP checkin of work on new parallax homepage

---
+++ js/parallax.js
@@ -0,0 +1,16 @@
+// Create cross browser requestAnimationFrame method:
+window.requestAnimationFrame = window.requestAnimationFrame
+ || window.mozRequestAnimationFrame
+ || window.webkitRequestAnimationFrame
+ || window.msRequestAnimationFrame
+ || function(f){setTimeout(f, 1000/60)}
+ 
+function parallaxScroll(){
+	var yoff = window.pageYOffset // get number of pixels document has scrolled vertically
+	var parallaxY = "center " + (-yoff * .1) + "px";
+	$("body").css("background-position", parallaxY);
+}
+ 
+window.addEventListener('scroll', function(){ // on page scroll
+	requestAnimationFrame(parallaxScroll)
+}, false)
