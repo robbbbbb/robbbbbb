cc8c547fe06f59d113cc0a18d2caf50247b2886e
Rob Minson <rob.minson@mindcandy.com>
2016-01-31 00:16:00 +0000

    Switching index page over to new site design

--- js/parallax-cb1.js
+++ js/parallax-cb1.js
@@ -4,12 +4,19 @@ window.requestAnimationFrame = window.requestAnimationFrame
  || window.webkitRequestAnimationFrame
  || window.msRequestAnimationFrame
  || function(f){setTimeout(f, 1000/60)}
- 
+
+
+var initialOffset = 0; 
+$(document).ready(function() {
+	initialOffset = parseInt($("body").css("background-position").split(" ")[1].replace("px", ""));
+});
+
 function parallaxScroll(){
 	if ($(window).width() > 1200)
 		return;
+
 	var yoff = window.pageYOffset // get number of pixels document has scrolled vertically
-	var parallaxY = "center " + (-yoff * .75 + 150) + "px";
+	var parallaxY = "center " + (-yoff * .8 + initialOffset) + "px";
 	$("body").css("background-position", parallaxY);
 }
  
