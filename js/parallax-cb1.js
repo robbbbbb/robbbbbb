c52d569e1ab73d744dcc7473db8b3e620ecdfe4a
Rob Minson <rob.minson@mindcandy.com>
2016-01-31 11:13:45 +0000

    Fixed parallax on iOS

--- js/parallax-cb1.js
+++ js/parallax-cb1.js
@@ -12,12 +12,18 @@ $(document).ready(function() {
 });
 
 function parallaxScroll(){
+	// disable all scrolling on cinema displays for performance reasons
 	if ($(window).width() > 1200)
 		return;
 
-	var yoff = window.pageYOffset // get number of pixels document has scrolled vertically
-	var parallaxY = "center " + (-yoff * .8 + initialOffset) + "px";
-	$("body").css("background-position", parallaxY);
+	// setup the inital vars for debugging later
+	var yoff = window.pageYOffset // get number of pixels document has scrolled vertically	
+	var parallaxMagnitude = 1.0 / 4.0; // 1-in-4 parallax
+	var parallaxY = (yoff * parallaxMagnitude + initialOffset) + "px"; // scale the total scrolled amount of the background's position
+	$("body").css("background-position", "center " + parallaxY);
+
+	var debug = "yoff: " + yoff + "<br />parallaxY: " + parallaxY + "<br />css: " + $("body").css("background-position");
+	$("#debug-box").html(debug);
 }
  
 window.addEventListener('scroll', function(){ // on page scroll
